cbb054592e40d2edb2a331fcdf1c8ba785cf85a4
Wander Amaral <wanderamaral013@gmail.com>
2024-08-01 22:06:48 -0300

    commit

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite/next";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
-import migrations from "../../drizzle/migrations";
+import migrations from "../../../drizzle/migrations";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 
 const DATABASE_NAME = "database.db";
